cdd9135c7356ae9b70697961930304d1124b1700
AlexanderHagegard <76807794+AlexanderHagegard@users.noreply.github.com>
2023-12-12 15:04:28 +0100

    Added device-menu javascript

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,5 @@
 var swiper = new Swiper(".swiper", {
+    loop: true, 
     slidesPerView: 3,
     spaceBetween: 30,
     pagination: {
@@ -9,6 +10,21 @@ var swiper = new Swiper(".swiper", {
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
     },
   });
 
